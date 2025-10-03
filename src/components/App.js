dc3317aadf0d475c781d6dcd586f47b46c06b5e2
saiakhilrevu03 <102013188+saiakhilrevu03@users.noreply.github.com>
2025-10-03 15:16:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import UserList from "./UserList";
 import UserDetails from "./UserDetails";
 
@@ -7,6 +7,8 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
+        {/* Redirect root path "/" to "/users" */}
+        <Route exact path="/" render={() => <Redirect to="/users" />} />
         <Route exact path="/users" component={UserList} />
         <Route path="/users/:id" component={UserDetails} />
       </Switch>
